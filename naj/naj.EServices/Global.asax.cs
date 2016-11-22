63e44f38139c1b60850099339191dd12be977d6f
mahmoud.shalsh <mahmoud.adel.shalsh@gmail.com>
2016-11-23 02:31:02 +0300

    Add www, Balady WebApp Add UI WebSite

--- naj/naj.EServices/Global.asax.cs
+++ naj/naj.EServices/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace naj.Web
+namespace naj.EServices
 {
     public class MvcApplication : System.Web.HttpApplication
     {
--- naj/naj.EServices/Properties/AssemblyInfo.cs
+++ naj/naj.EServices/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("naj.Web")]
+[assembly: AssemblyTitle("naj.EServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("naj.Web")]
+[assembly: AssemblyProduct("naj.EServices")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
